2b225e5df066fc3205d17215ab47bf655ee99b78
Velhsq <velhsq@inbox.ru>
2020-10-07 14:29:00 +0700

    Home work 4

--- lesson2/HW2.2.py
+++
@@ -1,8 +0,0 @@
-a = [5, 2, 's', 4.5, 4, (5, 7), 'r', 2, 5]
-
-
-for i in range(len(a)):
-    if i % 2 != 0:
-        a[i - 1], a[i] = a[i], a[i - 1]
-
-print(a)
--- lesson3/HW3.3.py
+++
@@ -1,9 +0,0 @@
-def my_func(a, b, c):
-    min_number = min(a, b, c)
-    return a + b + c - min_number
-
-
-print(my_func(100, 1000, 10000))
-
-
-
