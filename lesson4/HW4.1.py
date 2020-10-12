d74f5c5da4b0660282dc99959bb586dfc7ca4b0a
Velhsq <velhsq@inbox.ru>
2020-10-13 02:09:15 +0700

    added lesson5 home work

--- lesson4/HW4.1.py
+++
@@ -1,9 +0,0 @@
-from sys import argv
-
-def salary(working=float(argv[1]), hours=float(argv[2]), prize=float(argv[3])):
-    return working * hours + prize
-
-try:
-    print(salary())
-except ValueError:
-    print('не верный ввод')
